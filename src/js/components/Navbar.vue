b6d25c2fe0e028e2c209dccb3e7d31c848da859e
Brayam Valero <bvalerop@gmail.com>
2023-06-17 17:44:55 -0400

    Changing navbar logo name

--- src/js/components/Navbar.vue
+++ src/js/components/Navbar.vue
@@ -9,7 +9,7 @@ const { user } = storeToRefs(authStore)
 
 <template>
     <b-navbar toggleable="sm" type="dark" variant="dark">
-        <b-navbar-brand to="/">Logo</b-navbar-brand>
+        <b-navbar-brand to="/">Backup Patios</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
