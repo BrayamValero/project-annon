489d54ae1a8b431c4ed53505b37c572128e76a54
Brayam Valero <bvalerop@gmail.com>
2023-06-17 18:04:05 -0400

    Feat: minor changes added

--- src/js/pages/Index.vue
+++ src/js/pages/Index.vue
@@ -3,11 +3,8 @@
         <b-container class="my-5">
             <b-row class="justify-content-center">
                 <b-col cols="12">
-                    <h1>Homepage</h1>
-                    <p>Bienvenido a la App</p>
-                    <!-- Comparacion -->
-                    <b-button variant="brand"> Hello </b-button>
-                    <button class="btn btn-brand">Hello</button>
+                    <h1>Backup Los Patios</h1>
+                    <p>Bienvenido a la App de Gestion de Archivos</p>
                 </b-col>
             </b-row>
         </b-container>
